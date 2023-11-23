979cf52cce62751f4498dbcf5a30c43591e07203
212215 <212215@1220-007-22>
2023-11-23 10:42:32 +0300

    Добавьте файлы проекта.

---
+++ LocalMinMax/Program.cs
@@ -0,0 +1,28 @@
+﻿class Program
+{
+    static void Main()
+    {
+        int day = int.Parse(Console.ReadLine());
+        if (day < 3)
+        {
+            Console.WriteLine("Количество дней должно быть больше 3");
+            return;
+        }
+        int[] temp = new int[day];
+        for (int i = 0; i < day; i++)
+        {
+            temp[i] = int.Parse(Console.ReadLine());
+        }
+        for (int i = 1; i < day - 1; i++)
+        {
+            if (temp[i] > temp[i - 1] && temp[i] > temp[i + 1])
+            {
+                Console.WriteLine($"day {i+1} : {temp[i]} (max)");
+            }
+            if (temp[i] < temp[i - 1] && temp[i] < temp[i + 1])
+            {
+                Console.WriteLine($"day {i+1} : {temp[i]} (min)");
+            }
+        }
+    }
+}
